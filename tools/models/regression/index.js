8288aa536a1036b87253095e85b007b9988b97fa
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2024-06-26 17:18:16 +0200

    Add some commens and tests

--- tools/models/regression/index.js
+++ tools/models/regression/index.js
@@ -11,25 +11,30 @@ function load (fname) {
     }
 }
 
+(() => {
+    console.log("hey")
+})()
+
 var { data } = load("./qcdt.json")
-// var { data } = load("./qhdt.json")
+var { data } = load("./qhdt.json")
 
 // Convert dT to T cold
 data = data.map(([dt, q, th, i]) => [th-dt, q, th, i])
 
 // The condition for a filter by the current I
-const byCurrent = ([tc, q, th, i]) => i === 2.1
+const current = 2.1
+const byCurrent = ([tc, q, th, i]) => i === current 
 
 // Inputs
 const x = data
-    //.filter(byCurrent)
-    //.map(([tc, q, th, i]) => [tc, th, i])
-    .map(([tc, q, th, i]) => [tc, q, i])
+    .filter(byCurrent)
+    .map(([tc, q, th, i]) => [tc, th])
+    //.map(([tc, q, th, i]) => [tc, q, i])
 
 // Outputs
 const y = data
-    //.filter(byCurrent)
-    .map(([tc, q, th, i]) => [th])
+    .filter(byCurrent)
+    .map(([tc, q, th, i]) => [q])
 
 const mlr = new MLR(x, y)
 // Standard error for Qc:
@@ -37,13 +42,13 @@ const mlr = new MLR(x, y)
 // 0.32 Watt when the I isn't used for the model
 // 
 // Standard error for Qh:
-// 3.04 Watt when the I is used for ML
-// 4.25 Watt when the I isn't used for the model
+// 4.01 Watt when the I is used for ML
+// 0.26 Watt when the I isn't used for the model
 //
 // Standard error for Th:
 // 10.1 degrees C when the I is used for ML
 // 3.10 degrees C when the I isn't used for the model
-console.log(mlr.predict([27, 14.6, 2.1]))
-console.log(mlr.predict([50, 16.1, 2.1]))
+console.log(mlr.predict([27, 27, current]))
+console.log(mlr.predict([50, 50, current]))
 console.log(mlr.toJSON())
 

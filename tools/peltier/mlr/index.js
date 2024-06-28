f1fc7c947d9c8be240d60328158eb5518ef1b90f
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2024-06-28 16:38:32 +0200

    Move the Peltier model to a separate file

--- tools/peltier/mlr/index.js
+++ tools/peltier/mlr/index.js
@@ -1,15 +1,4 @@
-import MLR from "ml-regression-multivariate-linear"
-import fs from "fs"
-
-// Returns an object from a JSON file
-function load (fname) {
-    try {
-        const json = fs.readFileSync(fname, "utf8")
-        return JSON.parse(json)
-    } catch (e) {
-        console.error(e)
-    }
-}
+import { getQc, getQh, getTc, getTh } from "./model.js"
 
 // Config
 const config = {
@@ -26,74 +15,6 @@ config.q /= 2
 
 // The currents from the charts
 const currents = [0.7, 1.4, 2.1, 2.8]
-
-// Convert dT to T cold
-const convert = ([dt, q, th, i]) => [th-dt, q, th, i]
-
-const getTh = (function () {
-    // Get data from the Peltier's Qc=f(dT) chart
-    const source = load("./qcdt.json").data.map(convert)
-   
-    return function (q, tc, current) {
-        const data = source.filter(([tc, q, th, i]) => i == current)
-        const x = data.map(([tc, q, th, i]) => [tc, q]) // Inputs
-        const y = data.map(([tc, q, th, i]) => [th])  // Output
-        const mlr = new MLR(x, y) // Learn
-        // Debug: show error
-        // console.log("Th=f(Tc,Q)", mlr.toJSON().summary.regressionStatistics)
-        // Predict
-        const [ th ] = mlr.predict([tc, q, current])
-        return th
-    }
-})()
-
-const getTc = (function () {
-    // Get data from the Peltier's Qc=f(dT) chart
-    const source = load("./qcdt.json").data.map(convert)
-
-    return function (q, th, current) { 
-        const data = source.filter(([tc, q, th, i]) => i == current)
-        const x = data.map(([tc, q, th, i]) => [th, q]) // Inputs
-        const y = data.map(([tc, q, th, i]) => [tc]) // Outputs
-        const mlr = new MLR(x, y) // Learn
-        // Debug: show error
-        // console.log("Tc=f(Th,Q)", mlr.toJSON().summary.regressionStatistics)
-        const [ tc ] = mlr.predict([th, q, current])
-        return tc
-    }
-})()
-
-const getQc = (function () {
-    // Get data from the Peltier's Qc=f(dT) chart
-    const source = load("./qcdt.json").data.map(convert)
-
-    return function (tc, th, current) { 
-        const data = source.filter(([tc, q, th, i]) => i == current)
-        const x = data.map(([tc, q, th, i]) => [tc, th]) // Inputs
-        const y = data.map(([tc, q, th, i]) => [q]) // Outputs
-        const mlr = new MLR(x, y) // Learn 
-        // Debug: show error
-        // console.log("Qc=f(Tc,Th)", mlr.toJSON().summary.regressionStatistics)
-        const [ q ] = mlr.predict([tc, th])
-        return q 
-    }
-})()
-
-const getQh = (function () {
-    // Get data from the Peltier's Qh=f(dT) chart
-    const source = load("./qhdt.json").data.map(convert)
-
-    return function (tc, th, current) {
-        const data = source.filter(([tc, q, th, i]) => i == current)
-        const x = data.map(([tc, q, th, i]) => [tc, th]) // Inputs
-        const y = data.map(([tc, q, th, i]) => [q]) // Outputs
-        const mlr = new MLR(x, y) // Learn
-        // Debug: show error
-        // console.log("Qh=f(dT)", mlr.toJSON().summary.regressionStatistics)
-        const [ q ] = mlr.predict([tc, th, current])
-        return q 
-    }
-})()
 
 ;(() => {
     console.log("Config:", config)
